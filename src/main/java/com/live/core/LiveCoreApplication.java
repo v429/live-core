707b7ea5637f01f94c3c7b4291a3bebbceff37ea
v429 <v429god@163.com>
2024-08-27 17:03:00 +0800

    init spring

---
+++ src/main/java/com/live/core/LiveCoreApplication.java
@@ -0,0 +1,12 @@
+package com.live.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveCoreApplication.class);
+    }
+}
